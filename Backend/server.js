f2ed521f24af91ae29152b681fce318c9bfce377
NIRANJAN GANESH SHARMA <niranjansharma2626@gmail.com>
2026-07-30 14:44:44 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const liveSessionRoutes = require("./routes/liveSessionRoutes");
 
 const connectDB = require("./config/db");
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../")));
 
 app.get("/", (req, res) => {
 
